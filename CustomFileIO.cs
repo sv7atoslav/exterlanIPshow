a817983c83db73873c3f3a228c3f4091ee26c22f
Sv7atoslav <52704744+sv7atoslav@users.noreply.github.com>
2019-08-13 19:24:57 +0300

    upload a code and a program
    first sharing with community my source code and program with CLI for cmd.exe

---
+++ CustomFileIO.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace ShowIPtoConsole
+{
+    /// <summary>
+    /// Collection of methods with files;
+    /// Created by Sviatoslav Sudilovskiy.
+    /// </summary>
+    /// <description>
+    /// If You add this class to your project, 
+    /// You can work with files "out of box"
+    /// </description>
+    static class CustomFileIO
+    {
+        public static Encoding Current_enc {get{ return Encoding.Unicode;}}
+
+        const string Title = "external IP";
+        public enum SupportedExtensions { txt, html, fb2 };
+
+        public static string CorrectFileName(string file_name, SupportedExtensions wanted_extension){ return file_name+"."+wanted_extension; }
+
+        /// <summary>
+        /// try read file, if failed, return message of error
+        /// </summary>
+        /// <param name="file_name">text.txt</param>
+		/// <param name = "wanted_extension">selected extension</param>
+        /// <returns>line that contains useful info</returns>
+        public static string ReadContentFile(string file_name, SupportedExtensions wanted_extension)
+        {
+            try
+            {
+                byte index_of_content = 0;
+                switch (wanted_extension)
+                {
+                    case (SupportedExtensions.txt): index_of_content = 0; break;
+                    case (SupportedExtensions.html): index_of_content = 2; break;
+                    case (SupportedExtensions.fb2): index_of_content = 7; break;
+                }
+                return File.ReadAllLines(CorrectFileName(file_name, wanted_extension), Encoding.Unicode)[index_of_content];
+            }
+            catch (Exception e) { return "Reading failed!" + "\r\n" + e.GetType() + "\r\n" + e.Message; };
+        }
+
+        public static bool BrutforceWriteFile(string wanted_file_name, string content, SupportedExtensions wanted_extension)
+        {
+            switch (wanted_extension)
+            {
+                case (SupportedExtensions.txt): return BrutforceWriteFile(wanted_file_name, new[] { content }, wanted_extension);
+                case (SupportedExtensions.html): return BrutforceWriteFile(wanted_file_name, new[] { "<head><title>" + Title + "</title> <meta http-equiv=\"Content-Type\" content=\"text/html;charset=Windows-1251\"></head>", "<body>", content, "</body>" }, wanted_extension);
+                case (SupportedExtensions.fb2):
+                    {
+                        return BrutforceWriteFile(wanted_file_name,
+                            new[] { "<FictionBook>",
+                            " <description>",
+                            "  <book-title>",
+                            "   "+Title,
+                            "  </book-title>",
+                            " </description>",
+                            " <body><p>",
+                            content,
+                            " </p></body>",
+                            "</FictionBook>",
+                            }, wanted_extension);
+                    }
+                default: throw new NotSupportedException("Unknown format");
+            }
+        }
+
+
+        public static bool BrutforceWriteFile(string wanted_file_name, string[] content, SupportedExtensions wanted_extension)
+        {
+			try
+			{
+				File.WriteAllLines(CorrectFileName(wanted_file_name, wanted_extension), content, Current_enc);
+				return true;
+			} catch {return false;}
+        }
+
+        /* TODO: implement SoftWriteFile
+        /// <summary>
+        /// function that try write text to filename_17.txt
+        /// </summary>
+        /// <param name="wanted_file_name"></param>
+        /// <param name="content">strings into file</param>
+        /// <returns>is equal wanted name of file and real name of file</returns>
+        public static bool SoftWriteFile(string wanted_file_name, string[] content, SupportedExtensions wanted_extension)
+        {
+            string file_path = CorrectFileName(wanted_file_name, wanted_extension);
+            bool file_yet_exist = File.Exists(file_path);
+            File.WriteAllLines((file_yet_exist && !CustomIO.AskBoolQuestion("Rewrite yet existed file")) ? CustomIO.PleaseInput("new name of target file"):file_path, content, current_enc);
+            return !file_yet_exist;
+        }*/
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿/* Создано в SharpDevelop.
+ * Дата: 13.08.2019
+ * 
+ * Для изменения этого шаблона используйте меню "Инструменты | Параметры | Кодирование | Стандартные заголовки".
+ */
+using System;
+
+namespace ShowIPtoConsole
+{
+    class Program
+    {
+        public static string PrgTitle { get { return "ShowIP.exe"; } }
+
+        static string DetectOption(string key)
+        {
+            switch (key)
+            {
+                case ("-h"): case ("--help"): { return "help"; }
+                case ("-t"): case ("--txt"): { return "export txt"; }
+                case ("--html"): { return "export html"; }
+                case ("-f"): case ("--fb2"): { return "export fb2"; }
+                default: { return "undefined"; }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="command"></param>
+        /// <param name="ip"></param>
+        /// <returns>sukcessful or fail operation</returns>
+        static bool CompleteCommand(string command, string ip)
+        {
+            if (command == "undefined") return false;
+            if (command == "help")
+            {
+                Console.WriteLine("usage:");
+                Console.WriteLine(PrgTitle);
+                Console.WriteLine("\tShow ext. IP in this cmd");
+                Console.WriteLine(PrgTitle + " -h || " + PrgTitle + " --help");
+                Console.WriteLine("\tShow this help message");
+                Console.WriteLine(PrgTitle + " -t || " + PrgTitle + " --txt");
+                Console.WriteLine("\texport to ExternalIP.txt ");
+                Console.WriteLine(PrgTitle + " -f || " + PrgTitle + " --fb2");
+                Console.WriteLine("\tKaptain Obvious");
+                Console.WriteLine(PrgTitle + " --html");
+                Console.WriteLine("\tKaptain Obvious");
+                return true;
+            }
+            if (command.Contains("export"))
+            {
+                if (ip == "undefined") return false;//FIXME a.b.c.d ip4 format
+                var cse = CustomFileIO.SupportedExtensions.txt;
+                switch (command.Split(' ')[1])
+                {
+                    case ("txt"): cse = CustomFileIO.SupportedExtensions.txt; break;
+                    case ("html"): cse = CustomFileIO.SupportedExtensions.html; break;
+                    case ("fb2"): cse = CustomFileIO.SupportedExtensions.fb2; break;
+                }
+                CustomFileIO.BrutforceWriteFile("ExternalIP", ip, cse);
+                return true;
+            }
+            //I prevent in 1th line of function, that this line dont be execute
+            throw new NotImplementedException();
+        }
+
+        public static void Main(string[] args)
+        {
+            if (args.Length > 1)
+            {
+                Console.WriteLine("too many arguments!\nSee --help (-h)");
+                return;
+            }
+            string publicIp = "My public IP Addres is: " + (new System.Net.WebClient()).DownloadString("https://api.ipify.org");
+            if (args.Length == 0)
+            {
+                Console.WriteLine(publicIp);
+                System.Threading.Thread.Sleep(5 * 1000);// because user can click for launch program
+            }
+            else
+                if (!CompleteCommand(DetectOption(args[0]), publicIp))
+            		Console.WriteLine("runtime fail.\nSee --help");
+        }
+    }
+}
